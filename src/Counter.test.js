287feafaf6aea65ba11226fa92513eee04b70844
Martin Tan <martinwdtan@gmail.com>
2023-04-05 17:27:07 +0800

    Initialize project

---
+++ src/Counter.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Counter from './Counter';
+
+test('renders', () => {
+    render(<Counter />);
+});
+
+test('snapshot test', () => {
+    
+});
+
+test('increments', () => {
+
+});
+
+test('decrements', () => {
+    
+});
